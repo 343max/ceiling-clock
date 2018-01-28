dfd4c79d257d7757a635f326fe816bd29a66567a
Max von Webel <maxvw@fb.com>
2018-01-28 00:33:03 -0800

    no leading 0 in front of the hours

--- matrix.ino
+++ matrix.ino
@@ -106,7 +106,7 @@ void loop(void)
   time_t t = now() - 3600 * 9;
   char charBuf[100];
   char delimiter = second(t) % 2 == 0 ? ':' : ' ';
-  sprintf(charBuf, "%02u%c%02u", hour(t), delimiter, minute(t));
+  sprintf(charBuf, "%u%c%02u", hour(t), delimiter, minute(t));
   P.displayText(charBuf, PA_CENTER, 0, 0, PA_PRINT, PA_NO_EFFECT );
   while (!P.displayAnimate());
 }
